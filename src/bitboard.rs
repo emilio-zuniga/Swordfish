368ec53a18a94ec6cf2e95192974f2d70a76248d
CursedCactusJack <165201142+CursedCactusJack@users.noreply.github.com>
2024-09-07 21:15:17 -0500

    Added notes on bitboard to/from FEN functions

--- src/bitboard.rs
+++ src/bitboard.rs
@@ -42,9 +42,28 @@ impl Default for BitBoard {
 impl BitBoard {
     fn from_fen_string(fen: &str) -> Self {
         todo!()
+        //it might be a good idea to create a game manager that handles
+        //turn movement and captured pieces, as well as castling rights
+        //and the like, only passing in the board representation of the FEN
+        
     }
 
     fn to_fen_string(&self) -> String {
         todo!()
+        //active color - get whose turn it is to move {w, b}
+        //castling rights - castle-able sides {QKqk-}
+        //possible En Passant targets - E.P. rules:
+        //          1. capturing pawn must have adv 3 ranks to perfor
+        //          2. captured pawn must have moved 2 squares in one turn
+        //          3. capture must be performed on turn immediately after 
+        //             the pawn being captured moves
+        //          - if the above conditions are met, include the coordinate
+        //            behind the pawn that just moved 2 spaces 
+        //            {a single coord on 4th or 5th rank}
+        //halfmove clock - moves since the last piece capture/pawn adv {MAX 100}
+        //          - game drawn when a counter reaches 100
+        //fullmove number - number of completed turns (increments when black moves) {probably u32}
+
+
     }
 }
