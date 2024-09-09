c883edfd974113dc90cb96573fc6a7c9c2937298
CursedCactusJack <165201142+CursedCactusJack@users.noreply.github.com>
2024-09-09 15:43:19 -0500

    Just saving progress.

--- src/gamemaster.rs
+++ src/gamemaster.rs
@@ -3,46 +3,7 @@ use bitboard::BitBoard;
 
 /// This is a representation of a chess game and the various states of each element.
 pub struct GameManager {
-    castle_kingside_white: bool,
-    castle_queenside_white: bool,
-    castle_kingside_black: bool,
-    castle_queenside_black: bool,
-    //choose data type {String, Vec}? for En Passant targets
-    white_to_move: bool,
-    halfmoves: u32,
-    total_turns: u32,
-    bitboard: BitBoard,
-}
-
-impl Default for GameManager {
-    //GameManager was created to keep track of data about a game not
-    //represented by a bitboard, such castling rights, player's turn
-    //to move, and other data relevant to the game
-    fn default() -> Self {
-        GameManager {
-            castle_kingside_white: true,
-            castle_queenside_white: true,
-            castle_kingside_black: true,
-            castle_queenside_black: true,
-            //initialize to none
-            white_to_move: true,
-            halfmoves: 0,
-            total_turns: 0,
-            bitboard: BitBoard::default(),
-        }
-    }
-}
-
-impl GameManager {
-    fn from_fen_string(fen: &str) -> Self {
-        todo!();
-        //will need to pass the board representation part of the
-        //string to BitBoard::from_fen_string
-    }
-
-    fn to_fen_string(&self) -> String {
-        todo!();
-        //FEN Notes:
+    //FEN Notes:
         //active color - get whose turn it is to move {w, b}
         //castling rights - castle-able sides {QKqk-}
         //possible En Passant targets - E.P. rules:
@@ -59,5 +20,45 @@ impl GameManager {
 
         //will need to call bitboard.to_fen() and append data
         //GM has stored to the end of it
+    bitboard: BitBoard,
+    white_to_move: bool,
+    castling_rights: String,
+    en_passant_targets: Vec<String>,
+    halfmoves: u32,
+    fullmoves: u32,
+}
+
+impl Default for GameManager {
+    /// Constructs a new `GameMaster`, set to Chess's starting position
+    fn default() -> Self {
+        GameManager {
+            bitboard: BitBoard::default(),
+            white_to_move: true,
+            castling_rights: String::new(),
+            en_passant_targets: Vec::new(),
+            halfmoves: 0,
+            fullmoves: 0,
+        }
+    }
+}
+
+impl GameManager {
+    /// A utility method for generating a new `GameMaster` from a FEN string\
+    /// * `fen` - a `&str` representing a game's state in FEN
+    /// * `returns` - a `GameMaster` as generated from the FEN
+    fn from_fen_string(fen: &str) -> Self {
+        /* split fen into tokens
+         * grab board - pass it into BitBoard::from_fen_string();
+         * match next token - white_to_move = true if "w", false if "b", otherwise, default to true
+         * castling_rights = next toke 
+         * 
+         */
+        todo!();
+        //will need to pass the board representation part of the
+        //string to BitBoard::from_fen_string
+    }
+
+    fn to_fen_string(&self) -> String {
+        todo!();
     }
 }
