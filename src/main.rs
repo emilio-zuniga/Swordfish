fb48c2b93e8781d93d843885437f5f77c73d2819
CursedCactusJack <165201142+CursedCactusJack@users.noreply.github.com>
2024-12-19 17:22:29 -0600

    Setup for examining movegen with flamegraphs.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,19 @@ mod gamemanager;
 mod movetable;
 mod types;
 
+use std::time::Instant;
+
+use gamemanager::{legal_moves::perft::perft, GameManager};
+use movetable::{noarc::NoArc, MoveTable};
+
+
 fn main() {
-    
+    let table = NoArc::new(MoveTable::default());
+    let board = GameManager::default();
+
+    let now = Instant::now();
+    let nodes = perft(1, board, &table);
+    let later = now.elapsed();
+    println!("Elapsed: {:.4?}", later);
+    println!("Nodes searched: {}", nodes);
 }
